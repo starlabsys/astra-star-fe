459b29c5aeb7e5ac0af7301e166aae56bc301825
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-09-15 21:13:07 +0700

    feat: fixing bug middleware.ts

--- src/middleware/withAuth.ts
+++ src/middleware/withAuth.ts
@@ -1,35 +1,3 @@
-// import {
-//   NextFetchEvent,
-//   NextMiddleware,
-//   NextRequest,
-//   NextResponse,
-// } from "next/server";
-// import { cookies } from "next/headers";
-
-// export default function withAuth(
-//   middleware: NextMiddleware,
-//   requireAuth: string[] = [],
-// ) {
-//   return async (req: NextRequest, next: NextFetchEvent) => {
-//     const pathName = req.nextUrl.pathname;
-
-//     if (requireAuth.includes(pathName)) {
-//       const cookieStore = cookies();
-//       const token = cookieStore.get("token");
-
-//       if (!token) {
-//         const url = new URL("/login", req.url);
-
-//         url.searchParams.set("callbackUrl", encodeURI(req.url));
-
-//         return NextResponse.redirect(url);
-//       }
-//     }
-
-//     return middleware(req, next);
-//   };
-// }
-
 import {
   NextFetchEvent,
   NextMiddleware,
@@ -51,15 +19,17 @@ export default function withAuth(
     );
 
     if (isProtectedRoute) {
+      // Extract token from cookies
       const cookieStore = cookies();
       const token = cookieStore.get("token");
 
-      if (!token) {
-        const url = new URL("/login", req.url);
+      // If no token found or token is explicitly null
+      if (!token || !token.value) {
+        const loginUrl = new URL("/login", req.url);
 
-        url.searchParams.set("callbackUrl", encodeURI(req.url));
+        loginUrl.searchParams.set("callbackUrl", encodeURI(req.url));
 
-        return NextResponse.redirect(url);
+        return NextResponse.redirect(loginUrl);
       }
     }
 
