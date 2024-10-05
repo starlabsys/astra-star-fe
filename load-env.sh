b1a1a587f68a8ee3403890e7bd6cea98e2a4516e
QyraPentadakosta99 <142347562+mp-pentadakosta@users.noreply.github.com>
2024-10-06 03:32:13 +0700

    add .env

--- load-env.sh
+++ load-env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Memuat variabel dari file .env ke dalam environment
 set -a # Menyalakan mode ekspor otomatis
-. ./astra-star-fe/.env # Mengambil variabel dari file .env
+. .env # Mengambil variabel dari file .env
 set +a # Menonaktifkan mode ekspor otomatis
